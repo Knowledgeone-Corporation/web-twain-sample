fea47d0b8fee0b348c92b23c3efc86811adbf988
amelkonian-knowledgeonecorp <61130690+amelkonian-knowledgeonecorp@users.noreply.github.com>
2020-12-16 16:00:52 +1100

    Incremented Version to 1.0.1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,7 +5,7 @@
         fileUploadURL: document.location.origin + '/Home/UploadFile', //This is the service that the scanned document will be uploaded to when complete
         clientID: $("#ClientID").val(), //This is a way to identify the user who is scanning.  It should be unique per user.  Session ID could be used if no user logged in
         setupFile: document.location.origin + '/Home/DownloadSetup', //location of the insallation file if service doesn't yet exist
-        version: "1.0" //This is optional. This will check that the version of the service is the same as whats expected by the installer.  You can update this when there is a new release
+        version: "1.0.1" //This is optional. This will check that the version of the service is the same as whats expected by the installer.  You can update this when there is a new release
     });    
 });
 
