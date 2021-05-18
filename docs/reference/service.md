413f2b5973eccc0863f02fb21f191a74682b81af
amelkonian-knowledgeonecorp <61130690+amelkonian-knowledgeonecorp@users.noreply.github.com>
2021-05-18 16:16:45 +1000

    Updated sample to version 1.3.0 (#3)
    * Updated sample to version 1.3.0
    Updated documentation.
    
    * Moved api_reference
    
    * Updated documentation
    
    * Updated API documentation Uri
    
    * Display upload response.

---
+++ docs/reference/service.md
@@ -0,0 +1,7 @@
+## Using REST Service
+
+The WebTWAIN SDK exposes a REST API.
+
+The following diagram outlines how you should invoke API using the provided [documentation](https://knowledgeone-corporation.github.io/web-twain-sample/).
+
+![alt text](../images/apiflow.png "Service flow")
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,15 +4,20 @@
         viewButton: $("#viewBtn"), //This is optional. Specify a element that when clicked will view scanned document
         fileUploadURL: document.location.origin + '/Home/UploadFile', //This is the service that the scanned document will be uploaded to when complete
         clientID: $("#ClientID").val(), //This is a way to identify the user who is scanning.  It should be unique per user.  Session ID could be used if no user logged in
-        setupFile: document.location.origin + '/Home/DownloadSetup', //location of the insallation file if service doesn't yet exist
-        version: "1.0.2" //This is optional. This will check that the version of the service is the same as whats expected by the installer.  You can update this when there is a new release
-    });    
+        setupFile: document.location.origin + '/Home/DownloadSetup', //location of the installation file if service doesn't yet exist
+        version: "1.3.0", //This is optional. This will check that the version of the service is the same as whats expected by the installer.  You can update this when there is a new release
+        interfacePath: "", // This is optional if your application lives under a subdomain.
+    });
 });
 
 function K1ScanServiceComplete(data) {
-    $(".filename").val(data.FileName);
-    $(".filesize").text(data.SizeDisplay);
+    console.log(data);
+    $(".filename").val(data.filename);
+    $(".filesize").text(data.sizeDisplay);
     $(".filesize").show();
     $(".fileview").show();
     $("#saveBtn").removeAttr('disabled');
+
+    $("#uploadResponseOutput").text(JSON.stringify(data.uploadResponse, null, 4));
+    $("#uploadResponseDiv").show();
 }
